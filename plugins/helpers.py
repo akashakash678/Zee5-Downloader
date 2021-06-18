48e1f461229ced0d4c601ab2af7800dc634ac377
akashakash678 <83580768+akashakash678@users.noreply.github.com>
2021-06-18 17:17:54 +0530

    Update helpers.py

--- plugins/helpers.py
+++ plugins/helpers.py
@@ -15,12 +15,12 @@ from script import script
 
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:80.0) Gecko/20100101 Firefox/80.0",
-    "Referer":"https://www.zee5.com",
+    "Referer":"https://drm.zee5play.ml",
     "Accept":"*/*",
     "Accept-Encoding":"gzip, deflate, br",
     "Connection":"keep-alive",
     "Accept-Language":"en-US,en;q=0.9",
-    "Origin":"https://www.zee5.com",
+    "Origin":"https://drm.zee5play.ml",
     "sec-fetch-dest":"empty",
     "sec-fetch-mode":"cors",
     "sec-fetch-site":"same-site"
